a5c875717c21447a954b32f55dc2a3405465be7f
Yoojin Lee <leeyj255@naver.com>
2015-11-04 08:10:16 +0000

    version 1.0

---
+++ index.js
@@ -0,0 +1,24 @@
+// reference.
+// http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string
+
+function occurrences (string, subString, allowOverlapping) {
+
+  string += "";
+  subString += "";
+  if (subString.length <= 0) return (string.length + 1);
+
+  var n = 0,
+    pos = 0,
+    step = allowOverlapping ? 1 : subString.length;
+
+  while (true) {
+    pos = string.indexOf(subString, pos);
+    if (pos >= 0) {
+      ++n;
+      pos += step;
+    } else break;
+  }
+  return n;
+}
+
+module.exports = occurrences;
